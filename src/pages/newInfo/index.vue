dea8acd6b54ac81f513307e186de09913bf66c7f
whitt123 <1143340498@qq.com>
2019-02-13 20:13:00 +0800

    完成新闻详情页

--- src/pages/newInfo/index.vue
+++ src/pages/newInfo/index.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        这是新闻详情页----{{id}}
+    <div class="newInfo-container">
+        <h1 class="title">{{newInfo.title}}</h1>
+        <p class="subTitle">
+            <span>发表时间：{{newInfo.add_time | dateFormat}}</span>
+            <span>点击: {{newInfo.click}}次</span>
+        </p>
+        <hr>
+        <div class="content" v-html="newInfo.content"></div>
     </div>
 </template>
 
@@ -8,13 +14,36 @@
 export default {
     data (){
         return {
-            id: this.$route.params.id
+            id: this.$route.params.id,
+            newInfo: []
         }
     },
     created(){
+        this.getNewInfo();
     },
     methods: {
-
+        getNewInfo(){
+            this.$http.get('getnew/'+this.id).then(result => {
+                this.newInfo = result.body.message[0];
+            })
+        }
     }
 }
 </script>
+
+
+<style lang="less">
+    .newInfo-container{
+        padding: 0 5px;
+        .title{
+            text-align: center;
+            font-size: 16px;
+            color: red;
+            margin: 10px 0;
+        }
+        .subTitle{
+            display: flex;
+            justify-content: space-between
+        }
+    }
+</style>
